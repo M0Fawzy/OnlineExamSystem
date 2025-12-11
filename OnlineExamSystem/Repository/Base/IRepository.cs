bb39c584237e513648da00a090e380d2087f7040
Mohamed Fawzy <64755201+M0Fawzy@users.noreply.github.com>
2025-12-11 22:05:10 +0200

    Add files via upload

---
+++ OnlineExamSystem/Repository/Base/IRepository.cs
@@ -0,0 +1,11 @@
+﻿namespace Online_Exam_System.Repository.Base
+{
+    public interface IRepository<T> where T : class
+    {
+        IEnumerable<T> GetAll();
+        T FindById(int id);
+        void AddOne(T item);
+        void UpdateOne(T entity);
+        void DeleteOne(T item);
+    }
+}
---
+++ OnlineExamSystem/Repository/MainRepository.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Online_Exam_System.Data;
+using Online_Exam_System.Repository.Base;
+
+namespace Online_Exam_System.Repository
+{
+    public class MainRepository<T> : IRepository<T> where T : class
+    {
+        private readonly ApplicationDbContext context;
+
+        public MainRepository(ApplicationDbContext context)
+        {
+            this.context = context;
+        }
+        public void AddOne(T item)
+        {
+            context.Set<T>().Add(item);
+            context.SaveChanges();
+        }
+
+        public void DeleteOne(T item)
+        {
+            context.Set<T>().Remove(item);
+            context.SaveChanges();
+        }
+
+        public T FindById(int id)
+        {
+            return context.Set<T>().Find(id);
+        }
+
+        public IEnumerable<T> GetAll()
+        {
+            return context.Set<T>().ToList();
+        }
+
+        public void UpdateOne(T entity)
+        {
+            context.Entry(entity).State = EntityState.Modified;
+            context.SaveChanges();
+        }
+    }
+}
